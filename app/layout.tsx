3703d41607d2bdb8ae3f4decc0ea85607bcaa228
Skeita116 <saitou.keita0116@gmail.com>
2026-08-19 00:07:41 +0900

    Add OGP image

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,29 @@ export const metadata: Metadata = {
       "山形県内のライブカメラを地図から簡単にチェック。観光、空港、温泉、スキー場など山形の「いま」を見てみよう。",
     url: "https://yamagata-ima.vercel.app",
     siteName: "やまがたいま",
+
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "やまがたいま｜山形ライブカメラマップ",
+      },
+    ],
+
     locale: "ja_JP",
     type: "website",
   },
 
   twitter: {
     card: "summary_large_image",
+
     title: "やまがたいま｜山形の「いま」が見えるライブマップ",
+
     description:
       "山形県内のライブカメラを地図からチェックできるWebサービス。",
+
+    images: ["/opengraph-image.png"],
   },
 };
 
